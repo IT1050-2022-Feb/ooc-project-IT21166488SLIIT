68818ae9a37ceb8f9719759c9d2542bf54de32a3
Diwan Sachidu <45502921+IT21166488SLIIT@users.noreply.github.com>
2022-05-20 06:44:48 +0000

    Create and add Admin class and Movie class and their implementations

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "admin.h"
+#include "movie.h"
+using namespace std;
+
+int main()
+{
+
+    cout << "-- Admin Details --" << endl;
+	  Admin ad001 = Admin(1, "Diwan Sachidu", "diwansachidu", 
+    "diwansachidu@gmail.com");
+	  ad001.displayAdminDetails();
+
+    cout << "-- Movie Details --" << endl;
+    Movie m1 = Movie(1,"Sonic the Hedgehog 2", "Action", "Jeff Fowler", 
+    "Josh Miller,Patrick Casey,John Whittington", "Ben Schwartz,Idris 
+    Elba,Colleen O'Shaughnessey");
+    m1.displayMovieDetails();
+
+    cout << "-- Now Showing Movie Detials --" << endl;
+    NowShowingMovie n1 = NowShowingMovie(2, "Turning Red", "Animation", 
+    "Domee Shi", "Rosana Sullivan,Searit Huluf,Sarah Streicher", "Rosalie 
+    Chiang,Sandra Oh,Ava Morse", "2022-05-30");
+    n1.displayMovieDetails();
+
+    cout << "-- Comimg Soon Movie Detials --" << endl;
+    ComingSoonMovie c1 = ComingSoonMovie(3, "Jurassic World Dominion", 
+    "Adventure", "Colin Trevorrow", "Colin Trevorrow,Derek Connolly,Emily 
+    Carmichael", "Sam Neill,Laura Dern,Jeff Goldblum", "2022-07-10");
+    c1.displayMovieDetails();
+}
